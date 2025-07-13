20f4ca662275cf37b577a7936052119644ee1cc3
Anderson Babinski <andeerlb@gmail.com>
2025-07-13 19:17:08 -0300

    adler-32

--- checksums/adler-32.py
+++ checksums/adler-32.py
@@ -2,19 +2,20 @@
 import zlib
 
 string_to_hash = "Anderson"
-str_hash = zlib.crc32(string_to_hash.encode('utf-8'))
-print(f"hash of '{string_to_hash}': {str_hash}")
+hash = zlib.adler32(string_to_hash.encode('utf-8'))
+print(f"hash: {hash}")
+
 
 # %%
 import zlib
 
-file_hash = 0
+file_checksum = 0
 with open("../files/test_file.txt",'rb') as f:
     while True:
         chunk = f.read(4096)  # Read 4KB at a time
         if not chunk:
             break
-        file_hash = zlib.crc32(chunk)
+        file_checksum = zlib.adler32(chunk)
 
-print(f"hash of 'test_file.txt': {file_hash}")
+print(f"hash of 'test_file.txt': {file_checksum}")
 # %%
